0b2aa0b05c4401a64ae3d87a9a9a0353ad8ea2fa
CodeZhifty <juulstausholmoscar@gmail.com>
2021-11-11 09:44:10 +0100

    Foreach, instead of for loop for listing cyber tools

--- scripts/tools.js
+++ scripts/tools.js
@@ -33,13 +33,15 @@ function like(post){
 firebase.database().ref("CyberSecurityPosts/").on("value", function(CyberSecurityPosts_object){
     if(CyberSecurityPosts_object.numChildren() != 0){
         document.querySelectorAll("#postElements").forEach(function(element){element.remove()});
-        for (let i = 1; i < Object.values(CyberSecurityPosts_object.val()).length + 1; i++) {
-            const title = eval(`CyberSecurityPosts_object.val().post_${i}.title`);
-            const desc = eval(`CyberSecurityPosts_object.val().post_${i}.desc`);
-            const url = eval(`CyberSecurityPosts_object.val().post_${i}.url`);
-            const photoUrl = eval(`CyberSecurityPosts_object.val().post_${i}.photoUrl`);
-            const likes = eval(`CyberSecurityPosts_object.val().post_${i}.likes`);
-            const hasLiked = eval(`CyberSecurityPosts_object.val().post_${i}.hasLiked.users`);
+        console.log(CyberSecurityPosts_object.val(), typeof CyberSecurityPosts_object.val())
+        const keys = Object.keys(CyberSecurityPosts_object.val());
+        keys.forEach((key, index) => {
+            const title = eval(`CyberSecurityPosts_object.val().${key}.title`);
+            const desc = eval(`CyberSecurityPosts_object.val().${key}.desc`);
+            const url = eval(`CyberSecurityPosts_object.val().${key}.url`);
+            const photoUrl = eval(`CyberSecurityPosts_object.val().${key}.photoUrl`);
+            const likes = eval(`CyberSecurityPosts_object.val().${key}.likes`);
+            const hasLiked = eval(`CyberSecurityPosts_object.val().${key}.hasLiked.users`);
             const post = document.createElement("div");
             post.id = "postElements";
             post.classList = "text-center bg-light shadow p-4 rounded mb-5";
@@ -47,17 +49,17 @@ firebase.database().ref("CyberSecurityPosts/").on("value", function(CyberSecurit
                 if (user) {
                     // User is signed in
                     if(hasLiked.includes(user.displayName)){
-                        post.innerHTML = `<h2 style="color: black;">${title}</h2> <p style="color: black;">${desc}</p> <img src="${photoUrl}" style="max-height: 20vh; max-width: 30vw;"> <div class="100-w m-3"></div> <a href="${url}" target="_blank"> <button class="btn btn-dark">Continue to page</button> </a> <div class="100-w mt-4"></div> <a href="javascript:like('post_${i}')" id="likeButton" style="text-decoration: none; color: blue;"><i class="fas fa-thumbs-up" style="display: inline;"></i> <p id="dcodeLikeNumber" style="display: inline;">${likes}</p></a>`;
+                        post.innerHTML = `<h2 style="color: black;">${title}</h2> <p style="color: black;">${desc}</p> <img src="${photoUrl}" style="max-height: 20vh; max-width: 30vw;"> <div class="100-w m-3"></div> <a href="${url}" target="_blank"> <button class="btn btn-dark">Continue to page</button> </a> <div class="100-w mt-4"></div> <a href="javascript:like('${key}')" id="likeButton" style="text-decoration: none; color: blue;"><i class="fas fa-thumbs-up" style="display: inline;"></i> <p id="dcodeLikeNumber" style="display: inline;">${likes}</p></a>`;
                     }
                     else{
-                        post.innerHTML = `<h2 style="color: black;">${title}</h2> <p style="color: black;">${desc}</p> <img src="${photoUrl}" style="max-height: 20vh; max-width: 30vw;"> <div class="100-w m-3"></div> <a href="${url}" target="_blank"> <button class="btn btn-dark">Continue to page</button> </a> <div class="100-w mt-4"></div> <a href="javascript:like('post_${i}')" id="likeButton" style="text-decoration: none; color: black;"><i class="fas fa-thumbs-up" style="display: inline;"></i> <p id="dcodeLikeNumber" style="display: inline;">${likes}</p></a>`;
+                        post.innerHTML = `<h2 style="color: black;">${title}</h2> <p style="color: black;">${desc}</p> <img src="${photoUrl}" style="max-height: 20vh; max-width: 30vw;"> <div class="100-w m-3"></div> <a href="${url}" target="_blank"> <button class="btn btn-dark">Continue to page</button> </a> <div class="100-w mt-4"></div> <a href="javascript:like('${key}')" id="likeButton" style="text-decoration: none; color: black;"><i class="fas fa-thumbs-up" style="display: inline;"></i> <p id="dcodeLikeNumber" style="display: inline;">${likes}</p></a>`;
                     }
                 } 
                 else {
-                    post.innerHTML = `<h2 style="color: black;">${title}</h2> <p style="color: black;">${desc}</p> <img src="${photoUrl}" style="max-height: 20vh; max-width: 30vw;"> <div class="100-w m-3"></div> <a href="${url}" target="_blank"> <button class="btn btn-dark">Continue to page</button> </a> <div class="100-w mt-4"></div> <a href="javascript:like('post_${i}')" id="likeButton" style="text-decoration: none; color: black;"><i class="fas fa-thumbs-up" style="display: inline;"></i> <p id="dcodeLikeNumber" style="display: inline;">${likes}</p></a>`;
+                    post.innerHTML = `<h2 style="color: black;">${title}</h2> <p style="color: black;">${desc}</p> <img src="${photoUrl}" style="max-height: 20vh; max-width: 30vw;"> <div class="100-w m-3"></div> <a href="${url}" target="_blank"> <button class="btn btn-dark">Continue to page</button> </a> <div class="100-w mt-4"></div> <a href="javascript:like('${key}')" id="likeButton" style="text-decoration: none; color: black;"><i class="fas fa-thumbs-up" style="display: inline;"></i> <p id="dcodeLikeNumber" style="display: inline;">${likes}</p></a>`;
                 }
             });
             document.getElementById("CyberSecurityPosts").appendChild(post);
-        }
+        });
     }
 });
